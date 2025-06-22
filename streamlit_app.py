ba36b76c497c45e2a24baeaf7167d6e16716f060
everyoya <79091415+everyoya@users.noreply.github.com>
2025-06-22 23:53:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,31 @@
 import streamlit as st
 
-st.title('First Try')
+# Initialize session state to hold user input and navigation
+if 'page' not in st.session_state:
+    st.session_state.page = 'intro'
 
-st.write('Slaaplekker!!')
-st.write('Hello world!!!!!!!!')
-st.write('Hello world!!!!!!!!')
+if st.session_state.page == 'intro':
+    st.title("👋 Welcome to My First Streamlit App")
+
+    st.write("""
+    This simple app demonstrates basic navigation between pages using Streamlit.
+    
+    You’ll enter your name below and get a personalized welcome message on the next page!
+    """)
+
+    first_name = st.text_input("First Name")
+    last_name = st.text_input("Last Name")
+
+    if st.button("Continue"):
+        if first_name and last_name:
+            st.session_state.first_name = first_name
+            st.session_state.last_name = last_name
+            st.session_state.page = 'welcome'
+            st.experimental_rerun()  # refresh the app to move to next page
+        else:
+            st.warning("Please enter both your first and last name.")
+
+elif st.session_state.page == 'welcome':
+    st.title(f"🎉 Welcome, {st.session_state.first_name} {st.session_state.last_name}!")
+    st.write("You're now on the second page of the app. Glad to have you here!")
+
