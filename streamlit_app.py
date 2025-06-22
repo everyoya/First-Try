6654e14f60a30b872e0d26cc791d30a7b8b6482a
everyoya <79091415+everyoya@users.noreply.github.com>
2025-06-22 22:48:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ st.title('First Try')
 st.write('Hello world!')
 st.write('Hello world!!!!!!!!')
 st.write('Hello world!!!!!!!!')
+
+a=st.text_area('Type in the text_area and click copy')
+
+if st.button('Copy'):
+    pyperclip.copy(a)
+    st.success('Text copied successfully!')
